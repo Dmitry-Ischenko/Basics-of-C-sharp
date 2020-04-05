1c87478ce525fb6df6c1b3c101f2fdd5e003f402
dmitry <dmitry@dtech-ural.ru>
2020-04-05 12:37:09 +0500

    Первая задача решена

---
+++ lesson3/task1/ComplexClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class ComplexClass
+{
+    double im;
+    double re;
+    public ComplexClass()
+    {
+        im = 0;
+        re = 0;
+    }
+    public ComplexClass(double _im, double re)
+    {    
+        im = _im;
+        this.re = re;
+    }
+    public ComplexClass Plus(ComplexClass x2)
+    {
+        ComplexClass x3 = new ComplexClass();
+        x3.im = x2.im + im;
+        x3.re = x2.re + re;
+        return x3;
+    }
+    public ComplexClass Minus(ComplexClass x2)
+    {
+        ComplexClass x3 = new ComplexClass();
+        x3.im = im - x2.im;
+        x3.re = re - x2.re; 
+        return x3;
+    }
+    //  Пример произведения двух комплексных чисел
+    public ComplexClass Multi(ComplexClass x)
+    {
+        ComplexClass y = new ComplexClass();
+        y.im = re * x.im + im * x.re;
+        y.re = re * x.re - im * x.im;
+        return y;
+    }
+    public double Im
+    {
+        get
+        {
+            return im;
+        }
+        set
+        {
+            if (value >= 0) im = value;
+        }
+    }
+    public string ToString()
+    {
+        return re + "+" + im + "i";
+    }
+
+}
+
---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Выполнил Ищенко Дмитрий
+        //1.а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры;
+        //  б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+
+        // А)
+        Console.WriteLine("а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.");
+        ComplexStruct complex1;
+        complex1.re = 1;
+        complex1.im = 1;
+
+        ComplexStruct complex2;
+        complex2.re = 2;
+        complex2.im = 2;
+        Console.WriteLine("Комплексное число complex1: " + complex1.ToString());
+        Console.WriteLine("Комплексное число complex2: " + complex2.ToString());
+        ComplexStruct result = complex1.Plus(complex2);
+        Console.WriteLine("Результат сложения: " + result.ToString());
+        result = complex1.Multi(complex2);
+        Console.WriteLine("Результат произведения: " + result.ToString());
+        result = complex1.Minus(complex2);
+        Console.WriteLine("Результат вычитания: "+ result.ToString());
+        Console.WriteLine("---------------------------------");
+
+        // Б)
+        Console.WriteLine("б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.");
+        ComplexClass complexclass1 = new ComplexClass(1, 1);
+        ComplexClass complexclass2 = new ComplexClass(2, 2);
+        Console.WriteLine("Комплексное число complexclass1: " + complexclass1.ToString());
+        Console.WriteLine("Комплексное число complexclass2: " + complexclass2.ToString());
+        ComplexClass resultclass;
+        resultclass = complexclass1.Plus(complexclass2);
+        Console.WriteLine("Результат сложения: " + resultclass.ToString());
+        resultclass = complexclass1.Multi(complexclass2);
+        Console.WriteLine("Результат произведения: " + resultclass.ToString());
+        resultclass = complexclass1.Minus(complexclass2);
+        Console.WriteLine("Результат вычитания: " + resultclass.ToString());
+        Console.ReadKey();
+    }
+}
+struct ComplexStruct
+{
+    public double im;
+    public double re;
+    //  в C# в структурах могут храниться также действия над данными
+    public ComplexStruct Plus(ComplexStruct x)
+    {
+        ComplexStruct y;
+        y.im = im + x.im;
+        y.re = re + x.re;
+        return y;
+    }
+    public ComplexStruct Minus(ComplexStruct x)
+    {
+        ComplexStruct y;
+        y.im = im - x.im;
+        y.re = re - x.re;
+        return y;
+    }
+    //  Пример произведения двух комплексных чисел
+    public ComplexStruct Multi(ComplexStruct x)
+    {
+        ComplexStruct y;
+        y.im = re * x.im + im * x.re;
+        y.re = re * x.re - im * x.im;
+        return y;
+    }
+    public string ToString()
+    {
+        return re + "+" + im + "i";
+    }
+}
+
