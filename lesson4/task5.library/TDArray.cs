5f3d5d61f62052677bee541e9ede7a2971c85847
dmitry <dmitry@dtech-ural.ru>
2020-04-08 23:49:02 +0500

    Сделал 5ю задачу. При проверке других задач, нашел ошибки.

--- lesson4/task5.library/TDArray.cs
+++ lesson4/task5.library/TDArray.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.IO;
 
-namespace TDArray
+namespace TDArrayLib
 {
     public class TDArray
     {
@@ -19,18 +20,70 @@ namespace TDArray
             Random rand = new Random();
             for (int i=0;i<array.GetLength(0);i++)
             {
-                for(int j = 0; j < array.GetLength(1); i++)
+                for(int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = rand.Next(20);
                 }
             }
+        }
+        public TDArray(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException(String.Format("Файл не найден, работать дальше не буду", path), "path");
+            }
+            string[] arrayStr = File.ReadAllLines(path);
+            int row = 0;
+            int column = 0;
+            int serchMaxColumn = 0;
+            for (int i = 0; i < arrayStr.Length; i++)
+            {
+                for (int j = 0; j < arrayStr[i].Length; j++)
+                {
+                    if (arrayStr[i][j] == ' ')
+                    {
+                        serchMaxColumn++;
+                    }
+                }
+                if (serchMaxColumn > column)
+                {
+                    column = serchMaxColumn;
+                }
+                if (serchMaxColumn > 0)
+                {
+                    row++;
+                }
+                serchMaxColumn = 0;
+            }
+            array = new int[row, column + 1];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                string buffer = "";
+                int z = 0;
+                for (int j = 0; j < arrayStr[i].Length; j++)
+                {
+                    if (arrayStr[i][j] != ' ')
+                    {
+                        buffer += arrayStr[i][j];
+                    }
+                    if (arrayStr[i][j] == ' ' || j == arrayStr[i].Length - 1)
+                    {
+                        if (Int32.TryParse(buffer, out array[i, z]))
+                        {
+                            z++;
+                        }
+                        buffer = "";
+                    }
+                }
+            }
+
         }
         public int GetSum()
         {
             int summ = 0;
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     summ+=array[i, j];
                 }
@@ -42,7 +95,7 @@ namespace TDArray
             int summ = 0;
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     if (array[i, j] > num)
                     {
@@ -59,7 +112,7 @@ namespace TDArray
                 int min = array[0,0];
                 for (int i = 0; i < array.GetLength(0); i++)
                 {
-                    for (int j = 0; j < array.GetLength(1); i++)
+                    for (int j = 0; j < array.GetLength(1); j++)
                     {
                         if (min> array[i, j])
                         {
@@ -77,7 +130,7 @@ namespace TDArray
                 int max = array[0, 0];
                 for (int i = 0; i < array.GetLength(0); i++)
                 {
-                    for (int j = 0; j < array.GetLength(1); i++)
+                    for (int j = 0; j < array.GetLength(1); j++)
                     {
                         if (max < array[i, j])
                         {
@@ -88,6 +141,57 @@ namespace TDArray
                 return max;
             }
         }
-          
+        public void GetMaxIndex(out int row,out int column)
+        {
+            int max = array[0, 0];
+            row = 0;
+            column = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                        row = i;
+                        column = j;
+                    }
+                }
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0} ",array[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+        public void PrintToFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                //вернуть исключение
+            }
+            string[] arrayStr = new string[array.Length];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j==array.GetLongLength(1)-1)
+                    {
+                        arrayStr[i] += array[i, j].ToString();
+                    } 
+                    else
+                    {
+                        arrayStr[i] += array[i, j].ToString() + " ";
+                    }
+                }
+            }
+            File.WriteAllLines(path, arrayStr);
+        }
     }
 }
--- lesson4/task5/Program.cs
+++ lesson4/task5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TDArrayLib;
 class Program
 {
     static void Main(string[] args)
@@ -11,9 +12,23 @@ class Program
         //           возвращающий номер максимального элемента массива(через параметры, используя модификатор ref или out).
         //    **б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
         //   ***в) Обработать возможные исключительные ситуации при работе с файлами.
-        int[,] array = new int[5, 4];
-        Console.WriteLine("row:{0}", array.GetLength(0));
-        Console.WriteLine("column:{0}", array.GetLength(1));
+        string path = "array.txt";
+        int row=10, column=10;
+        TDArray myArray = new TDArray(row, column);
+        myArray.Print();
+        Console.WriteLine("Сумма всех элементов массива равана: {0}", myArray.GetSum());
+        Console.WriteLine("Сумма элементов массива больше 10 равана: {0}", myArray.GetSum(10));
+        Console.WriteLine("Минимальный элемент массива: {0}", myArray.GetMin);
+        Console.WriteLine("Максимальный элемент массива: {0}", myArray.GetMax);
+        myArray.GetMaxIndex(out row, out column);
+        Console.WriteLine("Индекс максиимального элемента строка - {0}, столбец - {1} (отсчет идет от 0, если забыли)",row,column);
+        Console.WriteLine("Сохраним все наше добро в файл {0}",path);
+        myArray.PrintToFile(path);
+        Console.WriteLine("------------------");
+        Console.WriteLine("А теперь созданим новый объект, и считаем в него значения из файла (нажмите любую кнопку, что бы продолжить)");
+        Console.ReadKey();
+        TDArray myArray2 = new TDArray(path);
+        myArray2.Print();
         Console.ReadKey();
     }
 }
