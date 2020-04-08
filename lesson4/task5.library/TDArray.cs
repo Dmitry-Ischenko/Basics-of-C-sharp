732f42cca508c7781a57faf292ad922b8e013e40
dmitry <dmitry@dtech-ural.ru>
2020-04-08 21:30:58 +0500

    в процессе)

--- lesson4/task5.library/TDArray.cs
+++ lesson4/task5.library/TDArray.cs
@@ -13,6 +13,81 @@ namespace TDArray
         //    **б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
         //   ***в) Обработать возможные исключительные ситуации при работе с файлами.
         int[,] array;
-        public TDArray(int string)
+        public TDArray(int row,int column)
+        {
+            array = new int[row, column];
+            Random rand = new Random();
+            for (int i=0;i<array.GetLength(0);i++)
+            {
+                for(int j = 0; j < array.GetLength(1); i++)
+                {
+                    array[i, j] = rand.Next(20);
+                }
+            }
+        }
+        public int GetSum()
+        {
+            int summ = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); i++)
+                {
+                    summ+=array[i, j];
+                }
+            }
+            return summ;
+        }
+        public int GetSum(int num)
+        {
+            int summ = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); i++)
+                {
+                    if (array[i, j] > num)
+                    {
+                        summ += array[i, j];
+                    }
+                }
+            }
+            return summ;
+        }
+        public int GetMin
+        {
+            get
+            {
+                int min = array[0,0];
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); i++)
+                    {
+                        if (min> array[i, j])
+                        {
+                            min = array[i, j];
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+        public int GetMax
+        {
+            get
+            {
+                int max = array[0, 0];
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); i++)
+                    {
+                        if (max < array[i, j])
+                        {
+                            max = array[i, j];
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+          
     }
 }
