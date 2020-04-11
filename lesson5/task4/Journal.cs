bd2ea68c7ae61014b422b62a29fd22b13769cb0c
dmitry <dmitry@dtech-ural.ru>
2020-04-11 14:35:37 +0500

    не едет

---
+++ lesson5/task4/Journal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Journal
+{
+    string firstNamePrivate;
+    string lastNamePrivate;
+    int[] gradesPrivate;
+    public Journal()
+    {
+        firstNamePrivate = "";
+        lastNamePrivate = "";
+        gradesPrivate = new int[10];
+    }
+    public string firstName
+    {
+        get
+        {
+            return firstNamePrivate;
+        }
+        set
+        {
+            firstNamePrivate = value;
+        }
+    }
+    public string lastName
+    {
+        get
+        {
+            return lastNamePrivate;
+        }
+        set
+        {
+            lastNamePrivate = value;
+        }
+    }
+    public int this[int index]
+    {
+    }
+    //public int[] grades
+    //{
+    //    get
+    //    {
+    //        return gradesPrivate;
+    //    }
+    //    set
+    //    {
+    //        gradesPrivate = value;
+    //    }
+    //}
+    //public int grades[int index] {
+
+    //public int grades[int index]
+    //{
+    //    get
+    //    {
+    //        return gradesPrivate[index];
+    //    }
+    //}
+}
+
