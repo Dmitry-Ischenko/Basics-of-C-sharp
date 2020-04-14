c85aa7c75d4163cce765341bc72306e47796afeb
dmitry <dmitry@dtech-ural.ru>
2020-04-14 17:55:55 +0500

    Поехали

---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Выполнил Ищенко Дмитрий
+        //Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+        //а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум.
+        //    Использовать массив(или список) делегатов, в котором хранятся различные функции.
+        //б) *Переделать функцию Load, чтобы она возвращала массив считанных значений.
+        //    Пусть она возвращает минимум через параметр(с использованием модификатора out). 
+
+        SaveFunc("data.bin", -100, 100, 0.5);
+        Console.WriteLine(Load("data.bin"));
+        Console.ReadKey();
+
+    }
+    public static double F(double x)
+    {
+        return x * x - 50 * x + 10;
+    }
+    public static void SaveFunc(string fileName, double a, double b, double h)
+    {
+        FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+        BinaryWriter bw = new BinaryWriter(fs);
+        double x = a;
+        while (x <= b)
+        {
+            bw.Write(F(x));
+            x += h;// x=x+h;
+        }
+        bw.Close();
+        fs.Close();
+    }
+    public static double Load(string fileName)
+    {
+        FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        BinaryReader bw = new BinaryReader(fs);
+        double min = double.MaxValue;
+        double d;
+        for (int i = 0; i < fs.Length / sizeof(double); i++)
+        {
+            // Считываем значение и переходим к следующему
+            d = bw.ReadDouble();
+            if (d < min) min = d;
+        }
+        bw.Close();
+        fs.Close();
+        return min;
+    }
+}
---
+++ lesson6/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Выполнил Ищенко Дмитрий
+        //Переделать программу Пример использования коллекций для решения следующих задач:
+        //а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+        //б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(*частотный массив);
+        //в) отсортировать список по возрасту студента;
+        //г) *отсортировать список по курсу и возрасту студента;
+
+        int bakalavr = 0;
+        int magistr = 0;
+        // Создадим необобщенный список
+        ArrayList list = new ArrayList();
+        // Запомним время в начале обработки данных
+        DateTime dt = DateTime.Now;
+        StreamReader sr = new StreamReader("..\\..\\students_1.csv");
+        while (!sr.EndOfStream)
+        {
+            try
+            {
+                string[] s = sr.ReadLine().Split(';');
+                // Console.WriteLine("{0}", s[0], s[1], s[2], s[3], s[4]);
+                list.Add(s[1] + " " + s[0]);// Добавляем склееные имя и фамилию
+                if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+            }
+            catch
+            {
+            }
+        }
+        sr.Close();
+        list.Sort();
+        Console.WriteLine("Всего студентов:{0}", list.Count);
+        Console.WriteLine("Магистров:{0}", magistr);
+        Console.WriteLine("Бакалавров:{0}", bakalavr);
+        foreach (var v in list) Console.WriteLine(v);
+        // Вычислим время обработки данных
+        Console.WriteLine(DateTime.Now - dt);
+        Console.ReadKey();
+    }
+}
