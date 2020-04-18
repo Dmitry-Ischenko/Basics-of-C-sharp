8ae07c358cbb6e56a28b8f0dd371063a58d1948a
dmitry <dmitry@dtech-ural.ru>
2020-04-18 10:23:33 +0500

    Первая задача готова

--- lesson7/task1/Form1.Designer.cs
+++ lesson7/task1/Form1.Designer.cs
@@ -32,11 +32,17 @@
             this.btnCommand2 = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
+            this.textCountAllClickButton = new System.Windows.Forms.Label();
+            this.ResultCountAllClickButton = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.StartGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(173, 12);
+            this.btnCommand1.Location = new System.Drawing.Point(215, 27);
             this.btnCommand1.Name = "btnCommand1";
             this.btnCommand1.Size = new System.Drawing.Size(75, 23);
             this.btnCommand1.TabIndex = 0;
@@ -46,7 +52,7 @@
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(173, 58);
+            this.btnCommand2.Location = new System.Drawing.Point(215, 56);
             this.btnCommand2.Name = "btnCommand2";
             this.btnCommand2.Size = new System.Drawing.Size(75, 23);
             this.btnCommand2.TabIndex = 1;
@@ -56,7 +62,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(173, 104);
+            this.btnReset.Location = new System.Drawing.Point(215, 114);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
@@ -68,22 +74,72 @@
             // 
             this.lblNumber.AutoSize = true;
             this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblNumber.Location = new System.Drawing.Point(33, 42);
+            this.lblNumber.Location = new System.Drawing.Point(12, 52);
             this.lblNumber.Name = "lblNumber";
             this.lblNumber.Size = new System.Drawing.Size(109, 39);
             this.lblNumber.TabIndex = 3;
             this.lblNumber.Text = "label1";
+            // 
+            // textCountAllClickButton
+            // 
+            this.textCountAllClickButton.AutoSize = true;
+            this.textCountAllClickButton.Location = new System.Drawing.Point(13, 139);
+            this.textCountAllClickButton.Name = "textCountAllClickButton";
+            this.textCountAllClickButton.Size = new System.Drawing.Size(38, 13);
+            this.textCountAllClickButton.TabIndex = 4;
+            this.textCountAllClickButton.Text = "Count:";
+            // 
+            // ResultCountAllClickButton
+            // 
+            this.ResultCountAllClickButton.AutoSize = true;
+            this.ResultCountAllClickButton.Location = new System.Drawing.Point(55, 139);
+            this.ResultCountAllClickButton.Name = "ResultCountAllClickButton";
+            this.ResultCountAllClickButton.Size = new System.Drawing.Size(13, 13);
+            this.ResultCountAllClickButton.TabIndex = 5;
+            this.ResultCountAllClickButton.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartGame});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(302, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // StartGame
+            // 
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(57, 20);
+            this.StartGame.Text = "Играть";
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(215, 85);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "button1";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(302, 164);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.ResultCountAllClickButton);
+            this.Controls.Add(this.textCountAllClickButton);
             this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(318, 203);
             this.MinimumSize = new System.Drawing.Size(318, 203);
@@ -91,6 +147,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +160,11 @@
         private System.Windows.Forms.Button btnCommand2;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label textCountAllClickButton;
+        private System.Windows.Forms.Label ResultCountAllClickButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem StartGame;
+        private System.Windows.Forms.Button btnBack;
     }
 }
 
--- lesson7/task1/Form1.cs
+++ lesson7/task1/Form1.cs
@@ -18,30 +18,65 @@ namespace task1
         //Игрок должен получить это число за минимальное количество ходов.
         //в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте обобщенный класс Stack.
         //Вся логика игры должна быть реализована в классе с удвоителем.
+        int _getStartResult = 0;
+        Stack<int> stack = new Stack<int>();
         public Form1()
         {
             InitializeComponent();
             btnCommand1.Text = "+1";
             btnCommand2.Text = "x2";
+            btnBack.Text = "Назад";
             btnReset.Text = "Сброс";
             lblNumber.Text = "0";
             this.Text = "Удвоитель";
         }
         private void btnCommand1_Click(object sender, EventArgs e)
         {
+            ResultCount();
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
         }
         private void btnCommand2_Click(object sender, EventArgs e)
         {
+            ResultCount();
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
         }
         private void btnReset_Click(object sender, EventArgs e)
         {
+            ResultCountAllClickButton.Text = "0";
             lblNumber.Text = "1";
+            stack.Clear();
+        }
+        private void ResultCount ()
+        {
+            ResultCountAllClickButton.Text = (int.Parse(ResultCountAllClickButton.Text) + 1).ToString();
+            stack.Push(int.Parse(lblNumber.Text));
+            if (int.Parse(lblNumber.Text) == _getStartResult)
+            {
+                MessageBox.Show("Вы выйграли! Вам понадобилось {0} ходов", ResultCountAllClickButton.Text);
+                ResultCountAllClickButton.Text = "0";
+            }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();            
+            _getStartResult = rand.Next(1, Int32.MaxValue);
+            MessageBox.Show($"Получите число {_getStartResult} за минимальное количество ходов");
+            ResultCountAllClickButton.Text = "0";
+            lblNumber.Text = "1";
+            stack.Clear();
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            if (stack.Count > 0)
+            {
+                lblNumber.Text = (stack.Pop()).ToString();
+            }
         }
     }
 
