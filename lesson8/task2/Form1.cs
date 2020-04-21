4421dbf25c5f3a03c5fd327dffb0d7a791d3a9fb
dmitry <dmitry@dtech-ural.ru>
2020-04-21 11:04:00 +0500

    Поехали

---
+++ lesson8/task2/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Выполнил Ищенко Дмитрий
+            //2. Используя полученные знания и класс TrueFalse, разработать игру «Верю — не верю».
+            InitializeComponent();
+        }
+    }
+}
---
+++ lesson8/task3/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Выполнил Ищенко Дмитрий
+            //3. *Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+            InitializeComponent();
+        }
+    }
+}
